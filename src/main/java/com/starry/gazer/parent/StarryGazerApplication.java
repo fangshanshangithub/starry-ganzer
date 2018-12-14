d2b954b3ed71932c4793458bea522578afa41647
fangshanshan <643915247@qq.com>
2018-12-14 18:12:20 +0800

    init project

---
+++ src/main/java/com/starry/gazer/parent/StarryGazerApplication.java
@@ -0,0 +1,14 @@
+package com.starry.gazer.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarryGazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarryGazerApplication.class, args);
+	}
+
+}
+
